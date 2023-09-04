d6e5e0d69d16d30b053446759006daeae0f40086
Joey <isjoeyful@gmail.com>
2023-09-04 20:04:16 +0900

    initiate project

---
+++ RamyeonLogoQuiz/RamyeonLogoQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  RamyeonLogoQuizApp.swift
+//  RamyeonLogoQuiz
+//
+//  Created by JeongAh Hong on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct RamyeonLogoQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
