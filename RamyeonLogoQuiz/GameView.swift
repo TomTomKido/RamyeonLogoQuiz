27205eb5d648e022e2ec33438a9521b0595e550b
Joey <isjoeyful@gmail.com>
2023-09-20 20:20:33 +0900

    GameView UI 큰 틀 완성

--- RamyeonLogoQuiz/GameView.swift
+++ RamyeonLogoQuiz/GameView.swift
@@ -11,32 +11,53 @@ struct GameView: View {
     var logo: Logo
     let parentSize: CGSize
     let ratio: CGFloat = 2 / 3
+    let blockSize: CGFloat = 50
 
     var body: some View {
         VStack {
-            Rectangle()
-                .frame(width: .infinity, height: 100)
-                .foregroundColor(.blue)
             quizImage
+                .padding(.vertical, 20)
+            answerBlock
+            lettersBlock
             Spacer()
         }
-        .background(Color.blue)
+        .background(Color.darkBlue)
     }
     
     var quizImage: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 6)
+            RoundedRectangle(cornerRadius: 10)
                 .foregroundColor(.white)
                 .frame(width: parentSize.width * ratio, height: parentSize.width * ratio)
                 .overlay {
                     RoundedRectangle(cornerRadius: 10)
-                        .stroke(.cyan, lineWidth: 10)
+                        .stroke(Color.accentBlue, lineWidth: 10)
                 }
             Image(logo.logoName)
                 .resizable()
                 .scaledToFill()
                 .frame(width: parentSize.width * ratio, height: parentSize.width * ratio)
                 .clipped()
+        }
+    }
+    
+    var answerBlock: some View {
+        LazyHGrid(rows: [.init(.fixed(blockSize))]) {
+            ForEach(0..<logo.letterCount, id: \.self) { _ in
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: blockSize, height: blockSize)
+                    .foregroundColor(.lightGray)
+            }
+        }
+    }
+    
+    var lettersBlock: some View {
+        LazyVGrid(columns: Array(repeating: .init(.fixed(blockSize)), count: 5)) {
+            ForEach(0..<10) { _ in
+                RoundedRectangle(cornerRadius: 5)
+                    .frame(width: blockSize, height: blockSize)
+                    .foregroundColor(.white)
+            }
         }
     }
 }
